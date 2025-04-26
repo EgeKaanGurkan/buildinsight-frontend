210cc2763ff80fc2c72de2f00c18f9f8438c9e17
EgeKaanGurkan <ege.gurkan2608@gmail.com>
2025-04-27 01:47:34 +0200

    make default dark theme

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -7,7 +7,7 @@ import {BuildInSightProvider, BuildInSight} from "buildinsight";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
       <BuildInSightProvider>
         <BuildInSight />
         <>{children}</>
